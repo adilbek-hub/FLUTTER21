a18433691e1122f65ad646b7ef55afac94bd8f18
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-07-24 23:33:31 +0600

    done

--- counter_app_second_page/lib/second_page.dart
+++ counter_app_second_page/lib/second_page.dart
@@ -7,7 +7,26 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('экинчи бет'),
+        centerTitle: true,
+        title: const Text('Тапшырма 2'),
+      ),
+      body: Center(
+        child: Container(
+          width: 325,
+          height: 44,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: const Color(0xffaaaaaa),
+          ),
+          child: const Center(
+              child: Text(
+            'Сан: 0',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+            ),
+          )),
+        ),
       ),
     );
   }
