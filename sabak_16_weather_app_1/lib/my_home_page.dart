d4066db8ca033b5953c34f1a5faec607bb9c5b37
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-11-29 00:09:51 +0600

    dddooon not

--- sabak_16_weather_app_1/lib/my_home_page.dart
+++ sabak_16_weather_app_1/lib/my_home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:sabak_16_weather_app_1/contants.dart/app_text_styles.dart';
 import 'package:sabak_16_weather_app_1/server.dart';
 import 'package:http/http.dart' as http;
 
@@ -42,19 +43,75 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Weather'),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xff66d8f1),
+        appBar: AppBar(
+          backgroundColor: Color(0xff66d8f1),
+          title: Icon(Icons.search),
+          actions: [
+            Icon(Icons.menu),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 31.5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Bishkek\nKyrgyzstan',
+                style: AppTextStyles.lacotionStyle,
+              ),
+              Text(
+                'Tue, Jun 30',
+                style: AppTextStyles.dataStyle,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset('assets/images/3222808.png', width: 150),
+                  Column(
+                    children: [
+                      Text(
+                        '$weatherInfo°',
+                        style: AppTextStyles.tempStyle,
+                      ),
+                      Text(
+                        'Rainy',
+                        style: AppTextStyles.tempNameStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              WeatherViewBanner(),
+              WeatherViewBanner(),
+              WeatherViewBanner(),
+            ],
+          ),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-                "$sityNAme шаарында абанын температурасы: $weatherInfo gradus"),
-          ],
+    );
+  }
+}
+
+class WeatherViewBanner extends StatelessWidget {
+  const WeatherViewBanner({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.31),
+      child: Container(
+        width: double.infinity,
+        height: 65.54,
+        decoration: BoxDecoration(
+          color: Colors.blueAccent,
+          borderRadius: BorderRadius.circular(
+            17.25,
+          ),
         ),
       ),
     );
