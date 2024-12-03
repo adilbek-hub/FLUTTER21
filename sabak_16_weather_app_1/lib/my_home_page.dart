bce87061b56342a1af2d57d45717058c17ad71b6
Adilbek <adilbek.kurmanbekuulu.kk@gmail.com>
2024-12-04 00:09:26 +0600

    hello

--- sabak_16_weather_app_1/lib/my_home_page.dart
+++ sabak_16_weather_app_1/lib/my_home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:sabak_16_weather_app_1/contants.dart/app_text_styles.dart';
 import 'package:sabak_16_weather_app_1/server.dart';
 import 'package:http/http.dart' as http;
+import 'package:sabak_16_weather_app_1/widgets/weather_view_banner.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -45,72 +46,94 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color(0xff66d8f1),
-        appBar: AppBar(
-          backgroundColor: Color(0xff66d8f1),
-          title: Icon(Icons.search),
-          actions: [
-            Icon(Icons.menu),
-          ],
+        backgroundColor: Color(
+          0xff66d8f1,
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 31.5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Bishkek\nKyrgyzstan',
-                style: AppTextStyles.lacotionStyle,
-              ),
-              Text(
-                'Tue, Jun 30',
-                style: AppTextStyles.dataStyle,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset('assets/images/3222808.png', width: 150),
-                  Column(
-                    children: [
-                      Text(
-                        '$weatherInfo°',
-                        style: AppTextStyles.tempStyle,
-                      ),
-                      Text(
-                        'Rainy',
-                        style: AppTextStyles.tempNameStyle,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              WeatherViewBanner(),
-              WeatherViewBanner(),
-              WeatherViewBanner(),
-            ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color(0xff66d8f1),
+                Color(
+                  0xff97eafc,
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class WeatherViewBanner extends StatelessWidget {
-  const WeatherViewBanner({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4.31),
-      child: Container(
-        width: double.infinity,
-        height: 65.54,
-        decoration: BoxDecoration(
-          color: Colors.blueAccent,
-          borderRadius: BorderRadius.circular(
-            17.25,
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 31.5, vertical: 20.7),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('assets/icons/search.png'),
+                    Image.asset('assets/icons/menu.png'),
+                  ],
+                ),
+                Text(
+                  'Bishkek,\nKyrgyzstan',
+                  style: AppTextStyles.lacotionStyle,
+                ),
+                Text(
+                  'Tue, Jun 30',
+                  style: AppTextStyles.dataStyle,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Image.asset(
+                      'assets/images/cludy.png',
+                      width: 250.16,
+                      height: 250.98,
+                      fit: BoxFit.fill,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              weatherInfo,
+                              style: AppTextStyles.tempStyle,
+                            ),
+                            Text(
+                              '\u2103',
+                              style: TextStyle(fontSize: 25),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'Rainy',
+                          style: AppTextStyles.tempNameStyle,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const WeatherViewBanner(
+                  image: 'assets/images/umbrella.png',
+                  text: 'RainFall',
+                  text2: '3cm',
+                ),
+                const WeatherViewBanner(
+                  image: 'assets/images/Vector.png',
+                  text: 'Wind',
+                  text2: '19km/h',
+                ),
+                const WeatherViewBanner(
+                  image: 'assets/images/Rectangle.png',
+                  text: 'Humidity',
+                  text2: '64%',
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ sabak_16_weather_app_1/lib/widgets/weather_view_banner.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class WeatherViewBanner extends StatelessWidget {
+  const WeatherViewBanner({
+    super.key,
+    required this.image,
+    required this.text,
+    required this.text2,
+  });
+  final String image, text, text2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.31),
+      child: Container(
+        width: double.infinity,
+        height: 65.54,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.25),
+          borderRadius: BorderRadius.circular(
+            17.25,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18.97, right: 48.29),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 37.94,
+                    height: 37.94,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(13.8),
+                    ),
+                    child: Image.asset(image),
+                  ),
+                  SizedBox(width: 17.46),
+                  Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 12.7,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                text2,
+                style: TextStyle(
+                  fontSize: 12.7,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
